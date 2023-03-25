e8565470f1063b8fea71b7639f9f6b9867a8e723
deximy <deximy@gmail.com>
2023-03-26 01:49:05 +0800

    feat: point the frontend output directory to the backend

--- SingleSignOn.Backend/Program.cs
+++ SingleSignOn.Backend/Program.cs
@@ -19,6 +19,9 @@ namespace SingleSignOn.Backend
 
             app.MapControllers();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.Run();
         }
     }
